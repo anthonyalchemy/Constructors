eaad2a6bbf2175f35dd762820d300d4e803a4618
anthonyalchemy <anthony@alchemytechs.com>
2019-07-31 14:21:03 +0100

    Add project files.

---
+++ Constructors/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors
+{
+    class Book
+    {
+        public string title; // class attributes 
+        public string author;
+        public int pages;
+
+        //now we can go to other prgram and create a book 
+
+        public Book(string aTitle, string aAuthor, int aPages) // constructor public then its the name of the class, special method called 
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //classes and ojects allow you to create custom data types
+            //classes model real world  entities 
+            /* Go to solution explorer 
+             * Click on program 
+             * add class 
+             * rename 
+             */
+
+            //object will be created here which is an instance of a class 
+
+            Book book1 = new Book("Harry Potter", "J.K.Rowling", 400); //book object (BOOK) is calling the constructor method 
+          
+            Book book2 = new Book("Lord of the rings", "Tolkein", 700); //book object
+
+            book2.title = "The Hobbit"; //changing the value of an attribute
+
+            Console.WriteLine(book2.title);
+            Console.ReadLine();
+
+        }
+    }
+}
